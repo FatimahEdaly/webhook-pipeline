adf8e97d508b5eceacc1bf5431138834acf5489f
FatimahEdaly <jalalfatimah977@gmail.com>
2026-03-24 00:57:00 +0200

    testing units

---
+++ src/tests/pipelines.test.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect, test } from "vitest";
+import {app} from "../server.js";
+import request from "supertest";
+describe("pipelines api", () => {
+
+  test("create pipeline", async () => {
+
+    const res = await request(app)
+      .post("/api/pipelines")
+      .send({
+        action: "upperCase",
+        subscribers: ["http://localhost:5000"]
+      });
+
+    expect(res.status).toBe(201);
+
+  });
+
+});
---
+++ src/tests/webhook.test.ts
@@ -0,0 +1,21 @@
+import request from "supertest";
+import {app} from "../server.js";
+import { describe, it, expect, test } from "vitest";
+
+describe("webhook", () => {
+
+  test("creates job successfully", async () => {
+
+    const pipelineId = "test-id";
+
+    const res = await request(app)
+      .post(`/webhook/${pipelineId}`)
+      .send({
+        payload: { name: "fatimah" }
+      });
+
+    expect(res.status).toBe(202);
+
+  });
+
+});
