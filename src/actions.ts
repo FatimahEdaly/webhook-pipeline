1dd1e50c4f48594ea04c88af8edb47d6e7093dd3
FatimahEdaly <jalalfatimah977@gmail.com>
2026-03-24 00:44:05 +0200

    processing action types

---
+++ src/actions.ts
@@ -0,0 +1,49 @@
+export function sortAsc(data:number[]){
+let temp;
+for(let i=0;i<data.length-1;i++){
+    for(let k=i+1;k<data.length;k++)
+       if(data[i]>data[k]){
+
+          temp=data[i];
+          data[i]=data[k];
+          data[k]=temp;
+        }
+
+
+}
+
+return data;
+
+}
+
+export function sortDesc(data:number[]){
+
+let temp;
+for(let i=0;i<data.length-1;i++){
+    for(let k=i+1;k<data.length;k++)
+       if(data[i]<data[k]){
+
+          temp=data[i];
+          data[i]=data[k];
+          data[k]=temp;
+        }
+
+
+}
+
+return data;
+}
+
+export function upperCase(data: Record<string, any>) {
+  const result: Record<string, any> = {};
+
+  for (const key in data) {
+    if (typeof data[key] === "string") {
+      result[key] = data[key].toUpperCase();
+    } else {
+      result[key] = data[key];
+    }
+  }
+
+  return result;
+}
