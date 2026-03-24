3df6d76424b010f3c04cc7e76d57299044c1714d
FatimahEdaly <jalalfatimah977@gmail.com>
2026-03-24 16:04:48 +0200

    modify webhook test

--- src/tests/webhook.test.ts
+++ src/tests/webhook.test.ts
@@ -2,16 +2,24 @@ import request from "supertest";
 import { app } from "../server.js";
 import { describe, expect, test } from "vitest";
 
+
+
 describe("webhook", () => {
   test("creates job successfully", async () => {
-    const pipelineId = "00000000-0000-0000-0000-000000000000";
+    const pipeline = await request(app)
+      .post("/pipelines")
+      .send({
+        name: "test pipeline",
+      });
+
+    const pipelineId = pipeline.body.id;
 
     const res = await request(app)
       .post(`/webhook/${pipelineId}`)
       .send({
         payload: { name: "fatimah" },
       });
-              
+
     expect(res.status).toBe(202);
   });
 });
